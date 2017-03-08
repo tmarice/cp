bfd9942e0e3df38fc24464e050d9b6fa62031571
Tomislav Maricevic <tomislav.maricevic@reversinglabs.com>
2017-03-08 14:48:57 +0100

    started x and his shots

---
+++ tracks/data_structures/advanced/x_and_his_shots.py
@@ -0,0 +1,13 @@
+
+n, m = [int(x) for x in raw_input().split()]
+
+shots = []
+for _ in range(n):
+    shots.append([int(x) for x in raw_input().split()])
+
+caught = []
+for _ in range(n):
+    caught.append([int(x) for x in raw_input().split()])
+
+
+
