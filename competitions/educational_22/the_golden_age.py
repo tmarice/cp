39ceb61d8df187265669e4e5dbda62d27d0a4803
Tomislav Maricevic <tmarice@gmail.com>
2017-06-06 22:00:28 +0200

    final the golden age

--- competitions/educational_22/the_golden_age.py
+++ competitions/educational_22/the_golden_age.py
@@ -3,38 +3,32 @@
 def main():
     x, y, l, r = [int(x) for x in raw_input().split()]
 
-    h = set()
-
-    f = min(x, y)
-    s = max(x, y)
+    h = set([l-1, r+1])
+    LIMIT = 10 ** 18
 
     for i in range(60):
+        a = x ** i
+        if a > LIMIT:
+            break
+
         for j in range(60):
-            t = s ** i + f ** j
-            h.add(t)
+            b = a + y ** j
+
+            if b > LIMIT:
+                break
+            else:
+                h.add(b)
 
     lt = sorted(h)
     check = []
 
     for x in lt:
-        if x >= l and x <= r:
+        if x >= l-1 and x <= r+1:
             check.append(x)
 
-    if len(check) == 0:
-        out = r - l + 1
-    elif len(check) == 1:
-        out = max(check[0] - l, r - check[0])
-    else:
-        out = 0
-        for i in range(len(check)):
-            if i == 0:
-                d = check[i] - l
-            elif i == len(check) - 1:
-                d = r - check[i]
-            else:
-                d = check[i+1] - check[i] - 1
-
-            out = max(out, d)
+    out = 0
+    for i in range(1, len(check)):
+        out = max(out, check[i] - check[i-1] - 1)
 
     print out
 
