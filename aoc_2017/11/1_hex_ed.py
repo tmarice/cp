05eaf42df560c22dfc15e20ab3ecd34ba6ce48e0
Tomislav Maricevic <tmarice@gmail.com>
2017-12-11 11:23:17 +0100

    advent of code 2017 day 11

---
+++ aoc_2017/11/1_hex_ed.py
@@ -0,0 +1,35 @@
+
+MOVES = {
+    "n": (0, 1, -1),
+    "ne": (1, 0, -1),
+    "se": (1, -1, 0),
+    "s": (0, -1, 1),
+    "sw": (-1, 0, 1),
+    "nw": (-1, 1, 0),
+}
+
+
+def trace(steps):
+    end =[0, 0, 0]
+
+    for step in steps:
+        dx, dy, dz = MOVES[step]
+
+        end[0] += dx
+        end[1] += dy
+        end[2] += dz
+
+    print(end)
+
+    return max(abs(x) for x in end)
+
+
+def main():
+    steps = input().strip().split(",")
+
+    n_s = trace(steps)
+    print(n_s)
+
+
+if __name__ == "__main__":
+    main()
